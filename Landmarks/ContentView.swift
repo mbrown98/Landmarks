983402997d6ae52169ad9dd882c11340947f29ff
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-08-04 17:10:15 -0400

    first

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Matthew Brown on 8/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matthew Brown on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
